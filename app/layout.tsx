0b19a72cd3bbd5ea87053edd6c7f7ac692b695a9
Shivansh <shivansh@codriva.com>
2025-12-09 15:33:49 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,11 @@ export const metadata: Metadata = {
         type: 'website',
         locale: 'en_US',
     },
- icons: {
+icons: {
   icon: [
-    { url: '/favicon.ico', sizes: '32x32', },
-  ],
-  apple: [
-    { url: '/favicon.ico', sizes: '180x180', type: 'image/png' }
+    { url: '/favicon.ico' },            // Chrome prefers this simple one
+    { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' }
   ]
 },
 
